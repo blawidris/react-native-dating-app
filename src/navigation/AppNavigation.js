747442ea8a53e63d6dceaad1aac6a342efe9d3da
blawidris <blawidris@gmail.com>
2024-01-09 23:37:18 +0100

    bottom navigation styling

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -8,6 +8,8 @@ import ChatDetailScreen from "../screens/ChatDetailScreen";
 import ChatScreen from "../screens/ChatScreen";
 import ProfileScreen from "../screens/ProfileScreen";
 import HomeScreen from "../screens/HomeScreen";
+import { Ionicons } from "@expo/vector-icons";
+import { widthPercentageToDP as wp } from "react-native-responsive-screen";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -15,10 +17,51 @@ const Tab = createBottomTabNavigator();
 const AppNavigation = () => {
   const HomeTabs = () => {
     return (
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          headerShown: false,
+          tabBarIcon: ({ focused }) => {
+            let iconName;
+
+            if (route.name === "home") {
+              iconName = "home";
+            } else if (route.name === "chat") {
+              iconName = "chatbubbles-outline";
+            } else if (route.name === "profile") {
+              iconName = "person-outline";
+            }
+
+            const customizeSize = 25;
+            return (
+              <Ionicons
+                name={iconName}
+                size={customizeSize}
+                color={focused ? "#3b82f6" : "gray"}
+              />
+            );
+          },
+          tabBarActiveTintColor: "#3b82fb",
+          tabBarInactiveTintColor: "gray",
+          tabBarStyle: {
+            backgroundColor: "white",
+            paddingTop:15,
+            paddingBottom: 15,
+            position: 'absolute',
+            bottom: 20,
+            left:20,
+            right: 20,
+            borderRadius: 50,
+            height: wp(20),
+          },
+          tabBarLabelStyle: {
+            fontWeight: "semibold",
+            fontSize: 16,
+          },
+        })}
+      >
         <Tab.Screen name="home" component={HomeScreen} />
         <Tab.Screen name="chat" component={ChatScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
+        <Tab.Screen name="profile" component={ProfileScreen} />
       </Tab.Navigator>
     );
   };
@@ -26,14 +69,17 @@ const AppNavigation = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="welcome"
+        initialRouteName="home"
         screenOptions={{ headerShown: false }}
       >
         <Stack.Screen name="welcome" component={WelcomeScreen} />
-        <Stack.Screen name="chatDetails" component={ChatDetailScreen} 
-        options={{
-          presentation: 'modal',
-        }}/>
+        <Stack.Screen
+          name="chatDetails"
+          component={ChatDetailScreen}
+          options={{
+            presentation: "modal",
+          }}
+        />
         <Stack.Screen name="home" component={HomeTabs} />
       </Stack.Navigator>
     </NavigationContainer>
